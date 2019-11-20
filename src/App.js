8f7f8ac7db2299659dd1c75583e08b916eb63694
ixnp <enprocopios@gmail.com>
2019-11-20 08:12:19 -0800

    fixed typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Counter from './component/Counter'
 import { createStore } from 'redux'
 
-const iinitialState = {
+const initialState = {
     count:0,
     message:''
 }
